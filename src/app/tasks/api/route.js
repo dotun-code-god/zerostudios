fe5ff16a07d124d24accefede564822507299330
dotun-code-god <dotun494@gmail.com>
2024-01-24 15:13:08 +0100

    removed ignored file from being imported

--- src/app/tasks/api/route.js
+++ src/app/tasks/api/route.js
@@ -1,5 +1,5 @@
 import { google } from "googleapis";
-import keys from '../../../../promptia-396220-e9972043fe80.json';
+// import keys from '../../../../promptia-396220-e9972043fe80.json';
 const CLIENT_EMAIL = process.env.NEXT_SERVICE_ACCOUNT_CLIENT_EMAIL;
 const PRIVATE_KEY = process.env.NEXT_SERVICE_ACCOUNT_PRIVATE_KEY;
 // const keys = {
